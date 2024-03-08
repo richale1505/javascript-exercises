8ebdd23105404537c1a3cf3148afdd9eb51d912e
Richard Farias <105022084+richale1505@users.noreply.github.com>
2024-03-08 00:34:32 -0400

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,32 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(grades) {
+  let conversionToCelsius = (grades - 32) / 1.8000;
+  let temp = 0;
+
+  if (Number.isInteger(conversionToCelsius)) {
+    temp = conversionToCelsius;
+  } else {
+    temp = Number(conversionToCelsius.toFixed(1));
+  }
+
+  console.log(temp + " Celsius");
+  return temp;
+  // C = F - 32 / 1.8000
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(grades) {
+  let conversionToFahr = (grades * 1.8) + 32;
+  let temp = 0;
+
+  if (Number.isInteger(conversionToFahr)) {
+    temp = conversionToFahr;
+  } else {
+    temp = Number(conversionToFahr.toFixed(1));
+  }
+
+  console.log(temp + " Fahrenheit");
+  return temp;
+
+  // F = (celsius * 1.8) + 32
 };
 
 // Do not edit below this line
