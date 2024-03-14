e66b1f716bc0c41729ca6115670541ba1c4cf55f
Richard Farias <105022084+richale1505@users.noreply.github.com>
2024-03-14 15:28:54 -0400

    solve excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(numOne, numbTwo) {
+	return numOne + numbTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let array = [];
+  for (let i = 0; i < power; i++) {
+    array.push(num);
+  } 
+  return array.reduce((total, item) => total * item);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let array = [];
+  let originalNum = num;
+  
+for (let i = 0; i < originalNum; i++) {
+  let numFactor = num--;
+  array.push(numFactor);
+}
+
+return array.reduce((total, item) => total * item, 1);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newArr = [];
+    let toLower = Array.from(str.replace(/[.,!]/g, "").toLowerCase().replace(/ /g, ''));
 
+    toLower.reduceRight((total, item) => newArr.push(item), 0);
+    
+    return newArr.toString().replace(/[,]/g, "") === toLower.toString().replace(/[,]/g, "");
 };
+
+/* Palindromes pseudocode
+    1. create a funtion that takes a string.
+    2. take that string and remove everything besided letter and number values
+    3. push char values to an array
+    4. reverse the position of the values in the array.
+    5. return true if equal and false otherwise
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
