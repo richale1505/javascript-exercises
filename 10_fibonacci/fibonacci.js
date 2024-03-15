005e7b715cd9e830491d4c746bb580309dac89eb
Richard Farias <105022084+richale1505@users.noreply.github.com>
2024-03-15 16:34:00 -0400

    Update files

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+function fibonacci(countArg) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
+    
+    if (count < 0) return 'OOPS';
+    if (count == 0) return 0;
+    
+    let firstNum = 1;
+    let secondNum = 0;
+    
+    for (let i = 2; i <= count; i++) {
+        let current = firstNum + secondNum;
+        secondNum = firstNum;
+        firstNum = current;
+    }
+    
+    return firstNum;
+}
 
-};
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, person) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentAge = getAge(person.yearOfDeath, person.yearOfBirth);
+        return oldestAge < currentAge ? person : oldest;
+    });
 };
+
+const getAge = function(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    } else {
+        return death - birth;
+    }
+      
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
